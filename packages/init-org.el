9feeb727e17e09a84ce20a2e6e43bf86c0d344b9
pedh <hcn518@gmail.com>
2020-12-04 14:56:17 +0800

    initialize org-mobile

--- packages/init-org.el
+++ packages/init-org.el
@@ -5,5 +5,8 @@
    (dot . t)))
 
 (setq org-confirm-babel-evaluate nil)
+(setq org-directory "~/org")
+(setq org-mobile-directory
+      "~/Library/Mobile Documents/iCloud~com~mobileorg~mobileorg/Documents")
 
 (provide 'init-org)
