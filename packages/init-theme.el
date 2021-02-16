64c902fdfb9f44abb218cf1db562526ead236f7c
pedh <hcn518@gmail.com>
2021-02-16 10:13:19 +0800

    enable smooth scrolling

--- packages/init-theme.el
+++ packages/init-theme.el
@@ -6,7 +6,16 @@
   :config
   (load-theme 'zenburn t))
 
+(use-package smooth-scrolling
+  :config
+  (smooth-scrolling-mode 1))
+
 (setq default-frame-alist '((font . "monaco-14")))
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
 
 (provide 'init-theme)
 ;;; init-theme.el ends here
